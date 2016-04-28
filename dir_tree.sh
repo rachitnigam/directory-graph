5a287390097b82d13b85a5be46c733bfae907ab1
Rachit Nigam <rnigam@umass.edu>
2016-04-28 16:20:43 -0400

    added sample image

--- dir_tree.sh
+++ dir_tree.sh
@@ -3,6 +3,7 @@
 set -eu
 
 FILE="$1"
+
 echo "strict digraph G {" > $FILE
 echo "rankdir=LR;">> $FILE
 find . | sed -E 's/([^/]+\/)*([^/]+\/[^/]+)$/\2/' | sed -E 's/([^/]*)(\/)?/"\1"\2/g' | sed 's|/| -> |g' | sed 's/$/;/' >> $FILE
